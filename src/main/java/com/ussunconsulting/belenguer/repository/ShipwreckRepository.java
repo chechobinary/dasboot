74edfbe7bc994698c3d44ba13725466238ecd0fb
Sergio Garcia Belenguer <sgarciabelenguer@gmail.com>
2017-09-30 12:49:44 +0100

    new version

--- src/main/java/com/ussunconsulting/belenguer/repository/ShipwreckRepository.java
+++ src/main/java/com/ussunconsulting/belenguer/repository/ShipwreckRepository.java
@@ -1,4 +1,9 @@
 package com.ussunconsulting.belenguer.repository;
 
-public interface ShipwreckRepository {
+
+import com.ussunconsulting.belenguer.model.Shipwreck;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
+
 }
